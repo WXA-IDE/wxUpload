ae65bbf58a168670daff0f43cbcbf2491a5befae
lyy <kajweb@163.com>
2018-02-27 13:56:47 +0800

    update to 0.9.2

--- wxUpload.php
+++ wxUpload.php
@@ -1,17 +1,20 @@
 <?php
 include 'lib/wxPacker.class.php';	//1.0.101
-include 'lib/wxUpload.class.php';	//0.9.102
+include 'lib/wxUpload.class.php';	//0.9.2
 
 $Input = "wxapp";
 $Output = "wxapp.wx";
-$appid = "wxd264b75bd1c77051";
-$newTicket = "7Fak-X4xaSgkkf6qYqN2BUBAIQJ8qJetocLl24xSsxs";
+$appid = "wx6fdfc32bedf1ecc8";
+$newTicket = "RH5MArvxPNAmgRAPC99S9TJ-yGwjGurHoSsmzOGjTe4";
+$userVersion = "1.0.2";
+$userDesc = "测试";
 
 $wxPacker = new wxPacker( $Input );
 $pack = $wxPacker->getPack();
 // $pack = $wxPacker->savePack( $Output );
 
 $a = new wxUpload( $newTicket, $appid );
-$src = $a->upload( $pack );
+$src = $a->upload( $pack, $userVersion, $userDesc );
+// $src = $a->perView( $pack );
 echo "<img src='{$src}' />";
 echo "<br>".$a->error;
